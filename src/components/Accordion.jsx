a615593734f27a7854ca7de0ec0521fb725da589
Saeed Dashti <saeed.da1994@gmail.com>
2025-10-04 23:56:52 +0330

    implement Accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,59 @@
+import {useState} from "react";
+import {ChevronDownIcon} from '@heroicons/react/24/outline'
+
 function Accordion() {
+
+    const data = [
+        {
+            id: 1,
+            title: 'Header One',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit doloribus iste repellendus, veritatis. Aliquid debitis odio optio sit voluptatem',
+        },
+        {
+            id: 2,
+            title: 'Header Two',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit doloribus iste repellendus, veritatis. Aliquid debitis odio optio sit voluptatem',
+        },
+        {
+            id: 3,
+            title: 'Header Three',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit doloribus iste repellendus, veritatis. Aliquid debitis odio optio sit voluptatem',
+        },
+        {
+            id: 4,
+            title: 'Header Four',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit doloribus iste repellendus, veritatis. Aliquid debitis odio optio sit voluptatem',
+        }
+    ]
+
     return (
-        <div >Hello from</div>
+        <div className="accordion">
+            {
+                data.map(item => <AccordionItem key={item.id} item={item}/>)
+            }
+        </div>
     )
 }
 
 export default Accordion;
+
+function AccordionItem({item}) {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className={`accordion-item ${isOpen ? 'accordion__expanded' : ''}`} key={item.id}>
+            <div className="accordion-item__header" onClick={() => setIsOpen(!isOpen)}>
+                <div>{item.title}</div>
+                <ChevronDownIcon style={
+                    {
+                        width: '1.2rem',
+                        transition: 'all .3s ease-out',
+                        rotate: isOpen ? '180deg' : '0deg'
+                    }
+                }/>
+            </div>
+            <div className="accordion-item__content">
+                {item.content}
+            </div>
+        </div>
+    )
+}
